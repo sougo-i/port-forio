a7d710bd39a4084bb6100433d07862531f22941f
伊東蒼梧 <>
2019-11-28 13:03:39 +0900

    ナビゲーションの追加
    ナビゲーションの追加

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(function() {
   setTimeout(function() {
     $('#lorde-fadein').fadeIn(1500);
   },500);
+  // ハンバーガーメニュー
   $('.hamburger').click(function() {
     $(this).hide(300);
     $('.menu').show(600);
@@ -13,4 +14,21 @@ $(function() {
     $('.menu').hide(600);
     $('.hamburger').show(600);
   });
+  // ナビゲーション
+  $('.wide-menu a').click(function() {
+    let href=$(this).attr('href');
+    let scroll=$(href).offset().top;
+    $('html,body').animate({'scrollTop':scroll},500);
+  })
+  // もっと見るボタン押した時に追加表示
+  $('#more').click(function() {
+    $('.moreWorks').slideToggle()
+    $(this).hide();
+    $('#more-2').show();
+  });
+  $('#more-2').click(function() {
+    $('.moreWorks').slideToggle()
+    $(this).hide();
+    $('#more').show();
+  })
 });
